e3b5f669ff5150c83745038df84881bdabcef879
Kip <iwakiri.kip@gmail.com>
2014-12-15 01:39:54 -0600

    Decoding string.

--- server.py
+++ server.py
@@ -18,4 +18,5 @@ sock.bind((ip, port))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    temp = data.decode()
     print("received message: ", data, "\nfrom: ", addr)
